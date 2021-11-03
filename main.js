d849a52f2583754f1b3bf5cd320ae4fe267122b8
022lm206 <82452130+022lm206@users.noreply.github.com>
2021-11-03 17:47:22 +0900

    넷바 메뉴 클릭 이동

--- main.js
+++ main.js
@@ -2,12 +2,37 @@
 
 // 넷바 색상 변경
 //let = 변수 (value), // const = 상수 (final)
-const navbar = document.querySelector; ('#navbar')
+const navbar = document.querySelector ('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener("scroll"), () => {
+document.addEventListener("scroll", () => {
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     } else {
-        navbar.classList.remove('navbar--datk');
+        navbar.classList.remove('navbar--dark');
     }
-}; // 이벤트
+}); // 이벤트
+
+// 넷바 메뉴 선택시 해당 부분으로 이동하기
+const navbarMenu = document.querySelector ('.navbar_menu');
+navbarMenu.addEventListener ("click", (event)=> {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    scrollIntoView(link);
+    console.log(link);
+    
+})
+
+// contact me 버튼 이동하기
+const home_contact = document.querySelector ('.home_contact');
+home_contact.addEventListener ("click", ()=> {
+    scrollIntoView('#contact');
+    
+})
+
+function scrollIntoView (selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
