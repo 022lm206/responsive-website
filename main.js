09270df8df7f214acb9f2f364c5e7307e81d9a67
022lm206 <82452130+022lm206@users.noreply.github.com>
2021-11-19 16:32:08 +0900

    My work

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 //넷바 색상 변경
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -9,24 +10,7 @@ document.addEventListener("scroll", () => {
     } else {
         navbar.classList.remove('navbar--dark');
     }
-});
-
-// arrow-up
-const arrow = document.querySelector(".arrow-up"); // arrow-up 클레스 선택 상수
-document.addEventListener("scroll",()=>{ // scroll 이벤트 발생시
-    if(window.scrollY < 1){
-        arrow.style.opacity = 0; // 처음 상태
-    }
-    if(window.scrollY > homeHeight/2){ // 홈 높이에서 절반 사라질시
-        arrow.style.opacity = 1;
-    }
-});
-
-// ?? 
-// function scrollIntoView(selector) {
-//     const arrowView = document.querySelector(selector);
-//     arrowView.scrollIntoView({ behavior: "smooth" });
-//     }
+})
 
 //넷바메뉴 선택시 해당 부분으로 이동하기
 const navbarMenu = document.querySelector(".navbar__menu");
@@ -36,49 +20,80 @@ navbarMenu.addEventListener("click", (event) => {
     if (link == null) {
         return;
     }
-    navbarMenu.classList.remove("open") ;
+    navbarMenu.classList.remove("open");
     scrollIntoView(link);
 });
 
 //contact me 버튼 이동하기
 const contactMeBtn = document.querySelector(".home__contact");
-contactMeBtn.addEventListener("click", ()=> {
+contactMeBtn.addEventListener("click", () => {
     scrollIntoView("#contact");
 });
 
-// arrow-up 버튼 이동하기
-const arrowupBtn = document.querySelector(".arrow-up");
-arrowupBtn.addEventListener("click", ()=> {
-    scrollIntoView("#home");
-});
-
-// home 투명도 조절
-
+//home 투명도 조절
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener("scroll" , ()=> { // 스크롤 이벤트 발생시
-    home.getElementsByClassName.opacity = 1 - window.screenY/homeHeight;
+document.addEventListener("scroll", ()=>{
+    home.style.opacity = 1 - window.scrollY/homeHeight;
 });
 
-// arrow-up scrollY
-const arrowup = document.querySelector('.arrow-up');
-home.addEventListener("scroll", () => {
-    if(window.scrollY > homeHeight/2){
-        arrowup.classList.add('arrowup--dark');
-    } else {
-        arrowup.classList.remove('arrowup--dark');
-    }
-});
-
-
-// 햄버거 메뉴 클릭할 때
+//햄버거 메뉴 클릭 시 
 const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
 navbarToggleBtn.addEventListener("click", ()=>{
     navbarMenu.classList.toggle("open");
 });
 
-// 스크롤시 부드럽게
+// arrow-up 버튼 스크롤 될 때 생성
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", ()=> {
+    if (window.scrollY > homeHeight/2) {
+        arrowUp.classList.add("visible");
+    } else {
+        arrowUp.classList.remove("visible");
+    }
+});
+
+// arrow-up 버튼 이동하기
+arrowUp.addEventListener("click", ()=> {
+    scrollIntoView("#home");
+});
+
+// 프로젝트 js 부분
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projects.forEach((project) => {
+    if (filter === "*" || filter === project.dataset.type) {
+      project.classList.remove("invisible");
+    } else {
+      project.classList.add("invisible");
+    }
+  });
+  const active = document.querySelector(".category__btn.selected");
+  if (active != null) {
+    active.classList.remove("selected");
+  }
+  e.target.classList.add("selected");
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
+});
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({ behavior: "smooth" });
-    }
+    scrollTo.scrollIntoView({behavior : "smooth"})
+}
